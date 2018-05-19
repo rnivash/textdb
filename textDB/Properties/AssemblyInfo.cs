b186a7f8c85168bf7c48941542832aa7eb16f61d
Nivash Ramachandran <nivash_ramachandran@dell.com>
2018-05-20 01:16:01 +0530

    Nuget package

--- textDB/Properties/AssemblyInfo.cs
+++ textDB/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("textDB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("TextDB")]
+[assembly: AssemblyDescription("Inline database based on text file")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("textDB")]
+[assembly: AssemblyProduct("TextDB")]
 [assembly: AssemblyCopyright("Copyright ©  2005")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
